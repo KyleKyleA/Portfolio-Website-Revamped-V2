7cfef3db9290c59c3636156532bcd5fed151728c
Kyle Angeles <kyle.angeles@dcmail.ca>
2026-05-08 15:44:14 -0400

    added some projects in the json for data since later on this website will be improvred for furture projects and when im working i can just add things on the side

---
+++ portfolio-v2/portfolio-v2/src/components/FlipCard.jsx
@@ -0,0 +1,6 @@
+// Author: Kyle Angeles
+// File: FlipCard.jsx
+// Date-Written: 5/8/26
+// Description: This component will be used to create flip card animations for the projects section of the website,
+// this will be used to display the project title and description on the front of the card and the project details on the back of the card, this will be a reusable component that can be used for all projects in the projects section of the website.
+// since this is a way of adding more projects to the projects.
--- portfolio-v2/portfolio-v2/src/views/Projects.jsx
+++ portfolio-v2/portfolio-v2/src/views/Projects.jsx
@@ -3,11 +3,12 @@
 // Date-Written: 4/29/26
 // Description: This section of the website will house my personal projects and class projects.
 
+
 function Projects() {
 
 
     return (
-
+        
         
         <><h2 className="mb-4 text-4xl font-bold tracking-tight text-heading md:text-5xl lg:text-6xl">List of Class and Personal Projects
         </h2>
@@ -17,6 +18,11 @@ function Projects() {
                 <button type="button" className="text-heading border border-buffer hover:border-default bg-neutral-primary focus:ring-4 focus:outline-none focus:ring-neutral-tertiary rounded-base text-base font-medium px-5 py-2.5 text-center me-3 mb-3">Search</button>
                 <button type="button" className="text-heading border border-buffer hover:border-default bg-neutral-primary focus:ring-4 focus:outline-none focus:ring-neutral-tertiary rounded-base text-base font-medium px-5 py-2.5 text-center me-3 mb-3">Sort By</button>
             </div>
+            
+
+            
+
+
             <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 <div>
                     <h2 className=""></h2>
